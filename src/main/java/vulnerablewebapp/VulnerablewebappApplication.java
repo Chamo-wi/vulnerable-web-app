e50d3091b9a4a2356d2caedace241c3c7aafd36c
Chamo-wi <chamowijekoon@gmail.com>
2026-03-18 21:37:58 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/vulnerablewebapp/VulnerablewebappApplication.java
@@ -0,0 +1,13 @@
+package vulnerablewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulnerablewebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VulnerablewebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vulnerablewebapp/VulnerablewebappApplicationTests.java
@@ -0,0 +1,13 @@
+package vulnerablewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulnerablewebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
